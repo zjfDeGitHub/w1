b34c8bda16b7243e5365e6fe0be849167dbe9621
zjfDeGitHub <44871005+zjfDeGitHub@users.noreply.github.com>
2018-12-21 14:05:22 +0800

    Update hongbao.php

--- hongbao/hongbao.php
+++ hongbao/hongbao.php
@@ -1,1 +1,138 @@
+<?php
+include_once("../weixinpaynew/WxPayPubHelper/WxPayPubHelper.php");
+require_once('hongbao.class.php');
+//http://vanke.lio8.com/yaohongbao/hongbao.php
+//发送红包
+/**
+ * 	作用：产生随机字符串，不长于32位
+ */
+function createNoncestr( $length = 32 )
+{
+    $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    $str ="";
+    for ( $i = 0; $i < $length; $i++ )  {
+        $str.= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+    }
+    return $str;
+}
 
+/**
+ * 	作用：格式化参数，签名过程需要使用
+ */
+function formatBizQueryParaMap($paraMap, $urlencode)
+{
+    $buff = "";
+    ksort($paraMap);
+    foreach ($paraMap as $k => $v)
+    {
+        if($urlencode)
+        {
+            $v = urlencode($v);
+        }
+        //$buff .= strtolower($k) . "=" . $v . "&";
+        $buff .= $k . "=" . $v . "&";
+    }
+    $reqPar;
+    if (strlen($buff) > 0)
+    {
+        $reqPar = substr($buff, 0, strlen($buff)-1);
+    }
+    return $reqPar;
+}
+
+/**
+ * 	作用：array转xml
+ */
+function arrayToXml($arr)
+{
+    $xml = "<xml>";
+    foreach ($arr as $key=>$val)
+    {
+        if (is_numeric($val))
+        {
+            $xml.="<".$key.">".$val."</".$key.">";
+
+        }
+        else
+            $xml.="<".$key."><![CDATA[".$val."]]></".$key.">";
+    }
+    $xml.="</xml>";
+    return $xml;
+}
+
+function getSign($Obj)
+{
+    foreach ($Obj as $k => $v)
+    {
+        $Parameters[$k] = $v;
+    }
+    //签名步骤一：按字典序排序参数
+    ksort($Parameters);
+    $String = formatBizQueryParaMap($Parameters, false);
+    //echo '【string1】'.$String.'</br>';
+    //签名步骤二：在string后加入KEY
+    $String = $String."&key=".KEY;
+    //echo "【string2】".$String."</br>";
+    //签名步骤三：MD5加密
+    $String = md5($String);
+    //echo "【string3】 ".$String."</br>";
+    //签名步骤四：所有字符转为大写
+    $result_ = strtoupper($String);
+    //echo "【result】 ".$result_."</br>";
+    return $result_;
+}
+
+
+
+
+//$xml = arrayToXml($datas);
+//	print_r(postXmlSSLCurl($xml,$dsourl)); //发送红包，已成功
+
+//http curl
+function postXmlSSLCurl($xml,$url,$second=30)
+{
+
+    //echo $xml;
+    $ch = curl_init();
+    //超时时间
+    curl_setopt($ch,CURLOPT_TIMEOUT,$second);
+    //这里设置代理，如果有的话
+    //curl_setopt($ch,CURLOPT_PROXY, '8.8.8.8');
+    //curl_setopt($ch,CURLOPT_PROXYPORT, 8080);
+    curl_setopt($ch,CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
+    //设置header
+    curl_setopt($ch,CURLOPT_HEADER,FALSE);
+    //要求结果为字符串且输出到屏幕上
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+    //设置证书
+    //使用证书：cert 与 key 分别属于两个.pem文件
+    //默认格式为PEM，可以注释
+    curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+    curl_setopt($ch,CURLOPT_SSLCERT,"D:\xwang".SSLCERT_PATH);
+    curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+    curl_setopt($ch,CURLOPT_SSLKEY,"D:\xwang".SSLKEY_PATH);
+    //post提交方式
+    curl_setopt($ch,CURLOPT_POST, true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$xml);
+    $data = curl_exec($ch);
+    //返回结果
+    if($data){
+        curl_close($ch);
+        return $data;
+    }
+    else {
+        $error = curl_errno($ch);
+        echo "curl出错，错误码:$error"."<br>";
+        echo "<a href='http://curl.haxx.se/libcurl/c/libcurl-errors.html'>错误原因查询</a></br>";
+        curl_close($ch);
+        return false;
+    }
+}
+
+
+
+
+
+?>
